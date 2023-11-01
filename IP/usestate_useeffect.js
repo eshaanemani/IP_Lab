73e4a3a52940380dfcae76cbaf83c271e9ae0322
Esha Nemani <123983362+eshaanemani@users.noreply.github.com>
2023-11-01 23:56:29 +0530

    usestate_useeffect.js

---
+++ IP/usestate_useeffect.js
@@ -0,0 +1,35 @@
+import React,{useState,useRef} from 'react'
+const AccessDOM = () => {
+const textAreaEl = useRef(null);
+const handleBtnClick = () => {
+textAreaEl.current.value =
+"Hello Everyone....Let's learn about React Hooks!!!";
+textAreaEl.current.focus();
+};
+return (
+<section style={{ textAlign: "center" }}>
+<div>
+<button onClick={handleBtnClick}>Focus and Populate 
+Text Field</button>
+</div>
+<label
+htmlFor="story"
+style={{
+display: "block",
+background: "olive",
+margin: "1em",
+padding: "1em"
+}}
+>
+The input box below will be focused and 
+populated with some text
+(imperatively) upon clicking the 
+button above.
+</label>
+<textarea ref={textAreaEl} id="story" 
+rows="5" cols="33" />
+</section>
+);
+};
+export default AccessDOM;
+
